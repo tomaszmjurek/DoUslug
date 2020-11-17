dfc61c915e768c958a974f5acec841ab7f7fe5fd
jurekt <tomasz.jurek@snpgroup.com>
2020-11-17 12:53:51 +0100

    Add basic calendar view

--- app/src/main/java/pp/inzynierka/douslug/MainActivity.kt
+++ app/src/main/java/pp/inzynierka/douslug/MainActivity.kt
@@ -1,11 +1,22 @@
 package pp.inzynierka.douslug
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this@MainActivity.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main)
+
+        CalendarBtn.setOnClickListener { openCalendarActivity() }
+    }
+
+    private fun openCalendarActivity() {
+        val intent = Intent(this@MainActivity, CalendarMonthActivity::class.java)
+        startActivity(intent)
     }
 }
