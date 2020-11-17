882c4326842a273a10df75a572299b693e3bd757
jurekt <tomasz.jurek@snpgroup.com>
2020-11-17 22:30:00 +0100

    Add nice looking button

---
+++ app/src/main/java/pp/inzynierka/douslug/CalendarMonthActivity.kt
@@ -0,0 +1,11 @@
+package pp.inzynierka.douslug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarMonthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_month)
+    }
+}
