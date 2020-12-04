221725d54b2c24a5f484a1dfad7c2c22c8ad3dcf
Chalkiss <filgap@wp.pl>
2020-12-04 18:17:28 +0100

    all clients view finished

--- app/src/main/java/pp/inzynierka/douslug/AllClientsActivity.kt
+++ app/src/main/java/pp/inzynierka/douslug/AllClientsActivity.kt
@@ -2,11 +2,27 @@ package pp.inzynierka.douslug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_all_clients.*
 
 
 class AllClientsActivity : AppCompatActivity() {
+    var dates = arrayOf(
+        "20.11.2020 12:30 - 13:30", "20.11.2020 13:30 - 14:30", "20.11.2020 14:30 - 16:00","20.11.2020 12:30 - 13:30", "20.11.2020 13:30 - 14:30", "20.11.2020 14:30 - 16:00","20.11.2020 12:30 - 13:30", "20.11.2020 13:30 - 14:30", "20.11.2020 14:30 - 16:00"
+        ,"20.11.2020 12:30 - 13:30", "20.11.2020 13:30 - 14:30", "20.11.2020 14:30 - 16:00"
+    )
+
+    var titles = arrayOf(
+        "John Lennon", "Maryla Rodowicz", "Krzysztof Ibisz","John Lennon", "Maryla Rodowicz", "Krzysztof Ibisz","John Lennon", "Maryla Rodowicz", "Krzysztof Ibisz","John Lennon", "Maryla Rodowicz", "Krzysztof Ibisz"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_clients)
+
+
+        val adapter = VisitListView(this, dates,  titles)
+        clients_list.adapter = adapter
     }
 }
---
+++ app/src/main/java/pp/inzynierka/douslug/AllClientsList.kt
@@ -0,0 +1,32 @@
+package pp.inzynierka.douslug
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class VisitListView // TODO Auto-generated constructor stub
+    (
+    private val context: Activity,
+    private val visitDate: Array<String>,
+    private val visitNotes: Array<String>
+) : ArrayAdapter<String?>(context, R.layout.activity_all_clients, visitNotes) {
+    override fun getView(
+        position: Int,
+        view: View?,
+        parent: ViewGroup
+    ): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.all_clients_list, null, true)
+        val dateText =
+            rowView.findViewById<View>(R.id.visitListDate) as TextView
+        val notesText =
+            rowView.findViewById<View>(R.id.visitListNotes) as TextView
+        dateText.text = visitDate[position]
+        notesText.text = visitNotes[position]
+
+        return rowView
+    }
+
+}
