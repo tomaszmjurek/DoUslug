0c97b682a5f43df78d28f49c5c6a6d8fd85dc191
jurekt <tomasz.jurek@snpgroup.com>
2020-12-27 20:56:34 +0100

    merge from develop

--- app/src/main/java/pp/inzynierka/douslug/calendar/CalendarDayActivity.kt
+++ app/src/main/java/pp/inzynierka/douslug/calendar/CalendarDayActivity.kt
@@ -7,11 +7,26 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.calendar_top_layout.*
 import kotlinx.android.synthetic.main.change_calendar_type.*
 import pp.inzynierka.douslug.R
+import pp.inzynierka.douslug.VisitListView
+
+var titles = arrayOf(
+    "John Lennon", "Maryla Rodowicz", "Krzysztof Ibisz"
+)
+var dates = arrayOf(
+    "20.11.2020 12:30 - 13:30", "20.11.2020 13:30 - 14:30", "20.11.2020 14:30 - 16:00"
+)
+
+var notes = arrayOf(
+    "włosy posklejane czymś czerwonym", "najpierw trzeba rozmrozić", "5 razy musisz mu wszystko potwierdzać"
+)
 
 class CalendarDayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_day)
+
+        val adapter = VisitListView(this, titles, dates, notes)
+        visits_list_view.adapter = adapter
 
         calendar_type_button.setOnClickListener { showCalendarChange() }
         day_button.setOnClickListener { showCalendarChange() }
--- app/src/main/java/pp/inzynierka/douslug/calendar/CalendarWeekActivity.kt
+++ app/src/main/java/pp/inzynierka/douslug/calendar/CalendarWeekActivity.kt
@@ -1,21 +1,35 @@
 package pp.inzynierka.douslug.calendar
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ArrayAdapter
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginBottom
 import kotlinx.android.synthetic.main.activity_calendar_week.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.calendar_top_layout.*
+import kotlinx.android.synthetic.main.calendar_week_element.view.*
 import kotlinx.android.synthetic.main.change_calendar_type.*
 import pp.inzynierka.douslug.R
 
 
 class CalendarWeekActivity : AppCompatActivity() {
 
+    private lateinit var mondayLayout : RelativeLayout
+    private lateinit var tuesdayLayout : RelativeLayout
+    private lateinit var wednesdayLayout : RelativeLayout
+    private lateinit var linearLayout: LinearLayout
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_week)
+
+        prepareLayout()
 
         calendar_type_button.setOnClickListener { showCalendarChange() }
         day_button.setOnClickListener { openCalendarDayActivity() }
@@ -23,10 +37,10 @@ class CalendarWeekActivity : AppCompatActivity() {
         month_button.setOnClickListener { openCalendarMonthActivity() }
         back_button.setOnClickListener { onBackPressed() }
 
-        var listItems = listOf<String>("element1", "element2", "element3")
+        var listItems = listOf("element1", "element2", "element3")
 
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems)
-        mondayListView.adapter = adapter
+//        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems)
+//        mondayListView.adapter = adapter
     }
 
     private fun showCalendarChange() {
@@ -47,5 +61,35 @@ class CalendarWeekActivity : AppCompatActivity() {
     private fun openCalendarMonthActivity() {
         val intent = Intent(this@CalendarWeekActivity, CalendarMonthActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun createWeekDayLayout(name: String, view: View, layout: LinearLayout) {
+//        val inflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+        view.weekName.text = name
+
+        var listView = ListView(this)
+        listView.setPadding(0, 0, 0, 20)
+//        listView.id = "@+id/listView"
+
+        layout.addView(view)
+        layout.addView(listView)
+
+    }
+
+    private fun prepareLayout() {
+        val weekNames = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+//        var view = layoutInflater.inflate(R.layout.calendar_week_element, findViewById(R.id.calendarWeekElement))
+        for (name in weekNames) {
+//            createWeekDayLayout(name, view, weekDaysLayout)
+            var view = LayoutInflater.from(this).inflate(R.layout.calendar_week_element, findViewById(mondayLayout.id))
+            view.weekName.text = weekNames[0]
+            weekDaysLayout.addView(view)
+            var dynamicView = TextView(this) //listView
+            dynamicView.setPadding(0, 0, 0, 20)
+            dynamicView.text = weekNames[1]
+            weekDaysLayout.addView(dynamicView)
+        }
+
+
     }
 }
