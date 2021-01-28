6339e87f5cd2988e77142eecdd7bc3641d0f5358
jurekt <tomasz.jurek@snpgroup.com>
2021-01-28 19:30:42 +0100

    Add payment reminder through SMS. Display money in polish format.

--- app/src/main/java/pp/inzynierka/douslug/FinanceActivity.kt
+++ app/src/main/java/pp/inzynierka/douslug/FinanceActivity.kt
@@ -1,6 +1,7 @@
 package pp.inzynierka.douslug
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -11,22 +12,28 @@ import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_finance.*
 import pp.inzynierka.douslug.adapters.NotPaidVisitAdapter
 import pp.inzynierka.douslug.calendar.DateConverter
+import pp.inzynierka.douslug.data.LoginHelper
 import pp.inzynierka.douslug.db.DBController
 import pp.inzynierka.douslug.model.Visit
+import java.text.DecimalFormat
+import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-class FinanceActivity : AppCompatActivity() {
+class FinanceActivity : AppCompatActivity(), NotPaidVisitAdapter.OnItemClickListener {
     private val TAG: String = "FINANCE_ACTIVITY"
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: NotPaidVisitAdapter
     private var monthlyRevenue: Double = 0.0
-
+    private val polishFormatter: NumberFormat = NumberFormat.getInstance(Locale.forLanguageTag("pl-PL"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finance)
+
+        polishFormatter.maximumFractionDigits = 2
+        polishFormatter.minimumFractionDigits = 2
 
         calculateMonthlyRevenue()
         setUpRecyclerView()
@@ -38,7 +45,7 @@ class FinanceActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.not_paid_visits_list)
         val visits = DBController.findNotPaidVisits()
         showNotPaidAmount(visits)
-        adapter = NotPaidVisitAdapter(visits)
+        adapter = NotPaidVisitAdapter(visits, this)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
@@ -52,7 +59,7 @@ class FinanceActivity : AppCompatActivity() {
         for(v in visits) {
             monthlyRevenue += v.service_id?.price!!
         }
-        monthly_revenue_text.text = "${getString(R.string.monthly_revenue)}     $monthlyRevenue"
+        monthly_revenue_text.text = "${getString(R.string.monthly_revenue)}     ${polishFormatter.format(monthlyRevenue)}"
     }
 
     private fun getMonthlyVisits() : RealmResults<Visit> {
@@ -67,6 +74,36 @@ class FinanceActivity : AppCompatActivity() {
         for (v in visits) {
             notPaidAmount += v.service_id?.price!!
         }
-        to_settle_text.text = "${getString(R.string.to_settle)}       $notPaidAmount"
+        to_settle_text.text = "${getString(R.string.to_settle)}       ${polishFormatter.format(notPaidAmount)}"
+    }
+
+    override fun onItemClick(position: Int) {
+        val visitForReminder = adapter.getItem(position)
+        if (visitForReminder != null){
+            remindAboutVisit(visitForReminder)
+        }
+    }
+
+    private fun remindAboutVisit(visit: Visit) {
+        val serviceName = visit.service_id?.name
+        val date = DateConverter.timestampToDateString(visit.date)
+        val phone = visit.client_id?.phone_num
+        val price = visit.service_id?.price
+        val userName = LoginHelper.getLoggedUserOrLogout(this)?.official_name
+
+        if (serviceName != null && phone != null && date != null && price != null && userName != null) {
+            val smsBody = prepareSmsBody(serviceName, date, price, userName)
+            val sendIntent = Intent(Intent.ACTION_VIEW)
+            sendIntent.data = Uri.parse("smsto:$phone")
+            sendIntent.putExtra("sms_body", smsBody)
+//            sendIntent.data = Uri.parse("smsto:$phone")
+            startActivity(sendIntent)
+        }
+    }
+
+    private fun prepareSmsBody(serviceName: String, date: String, price: Double, userName: String) : String {
+        val loggedUser = LoginHelper.getLoggedUserOrLogout(this)
+        return "Przypomnienie o nieopłaconej wizycie \"$serviceName\" z dnia: $date na kwotę ${polishFormatter.format(price)} zł." +
+            "\n\n~ $userName"
     }
 }
--- app/src/main/java/pp/inzynierka/douslug/adapters/NotPaidVisitAdapter.kt
+++ app/src/main/java/pp/inzynierka/douslug/adapters/NotPaidVisitAdapter.kt
@@ -1,13 +1,10 @@
 package pp.inzynierka.douslug.adapters
 
-import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.OrderedRealmCollection
 import io.realm.RealmRecyclerViewAdapter
@@ -16,7 +13,8 @@ import pp.inzynierka.douslug.calendar.DateConverter
 import pp.inzynierka.douslug.model.Visit
 
 
-internal class NotPaidVisitAdapter(data: OrderedRealmCollection<Visit>) : RealmRecyclerViewAdapter<Visit, NotPaidVisitAdapter.TaskViewHolder?>(data, true) {
+internal class NotPaidVisitAdapter(data: OrderedRealmCollection<Visit>, private val listener: OnItemClickListener)
+    : RealmRecyclerViewAdapter<Visit, NotPaidVisitAdapter.TaskViewHolder?>(data, true) {
     lateinit var _parent: ViewGroup
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -30,14 +28,28 @@ internal class NotPaidVisitAdapter(data: OrderedRealmCollection<Visit>) : RealmR
         val obj: Visit? = getItem(position)
         holder.data = obj
         holder.title.text = "${obj?.service_id?.name}: ${obj?.client_id?.first_name} ${obj?.client_id?.last_name}"
-        holder.text1.text = DateConverter.timestampToDateString(obj!!.date)
-//        holder.button.setOnClickListener {  }
+        holder.text1.text = DateConverter.combineTimestampWithDuration(obj?.date, obj?.service_id?.duration_min)
     }
 
-    internal inner class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    internal inner class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
         var data: Visit? = null
         var title: TextView = view.findViewById(R.id.itemTitle)
         var text1: TextView = view.findViewById(R.id.itemText1)
         var button: Button = view.findViewById(R.id.itemButton)
+
+        init{
+            button.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if(position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
     }
 }
