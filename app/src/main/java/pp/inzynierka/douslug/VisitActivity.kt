1cabe60c6016ab8f5e999777798e4c43a24f2a00
jurekt <tomasz.jurek@snpgroup.com>
2021-02-10 22:18:47 +0100

    Add opening proper client and service in VisitActivity

--- app/src/main/java/pp/inzynierka/douslug/VisitActivity.kt
+++ app/src/main/java/pp/inzynierka/douslug/VisitActivity.kt
@@ -11,10 +11,12 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_visit.*
 import pp.inzynierka.douslug.calendar.DateConverter
 import pp.inzynierka.douslug.db.DBController
+import pp.inzynierka.douslug.model.Visit
 
 class VisitActivity : AppCompatActivity() {
     private val TAG = "VISITS_ACTIVITY"
     private var editMode: Boolean = false
+    private lateinit var visit : Visit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,17 +38,14 @@ class VisitActivity : AppCompatActivity() {
 
         val visitID: String? = intent.getStringExtra("visitID")
         if (visitID != null) {
-            var visit = DBController.findVisitById(visitID)
-            if (visit != null) {
-                //fields[0].setText(visit.client_id?.first_name + " " +  visit.client_id?.last_name)
-                //fields[1].setText(visit.service_id?.name)
-                // TODO: wyświetl klienta i usługę zgodnie z otrzymanymi danymi
-                fields[0].setText(DateConverter.timestampToDateString(visit.date!!))
-                fields[1].setText(visit.service_id?.duration_min.toString())
-                fields[2].setText(visit.client_id?.address)
-                fields[3].setText(visit.service_id?.price.toString())
-                fields[4].setText(visit.client_id?.comment)
-            }
+            visit = DBController.findVisitById(visitID)!!
+            //fields[0].setText(visit.client_id?.first_name + " " +  visit.client_id?.last_name)
+            //fields[1].setText(visit.service_id?.name)
+            fields[0].setText(DateConverter.timestampToDateString(visit.date!!))
+            fields[1].setText(visit.service_id?.duration_min.toString())
+            fields[2].setText(visit.client_id?.address)
+            fields[3].setText(visit.service_id?.price.toString())
+            fields[4].setText(visit.client_id?.comment)
         }
 
 
@@ -106,12 +105,15 @@ class VisitActivity : AppCompatActivity() {
         val clients = DBController.findAllClients()
 
         val clientsNames = mutableListOf<String>()
-        for (c in clients) {
+        var clientPosition= 0
+        for ((i, c) in clients.withIndex()) {
             clientsNames.add("${c.first_name} ${c.last_name}")
+            if (c == visit.client_id) clientPosition = i
         }
 
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, clientsNames)
         clients_spinner.adapter = adapter
+        clients_spinner.setSelection(clientPosition)
         clients_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
@@ -130,12 +132,15 @@ class VisitActivity : AppCompatActivity() {
         val services = DBController.findAllServices()
 
         val servicesNames = mutableListOf<String>()
-        for (s in services) {
+        var servicePosition = 0
+        for ((i, s) in services.withIndex()) {
             servicesNames.add(s.name)
+            if (s == visit.service_id) servicePosition = i
         }
 
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, servicesNames)
         services_spinner.adapter = adapter
+        services_spinner.setSelection(servicePosition)
         services_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
